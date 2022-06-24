c487038a789ed843179ba0f5b9bbc1d79979d05b
Wagner Rose <wagner.rose@gmail.com>
2022-06-24 13:34:30 -0300

    Configuração do Bootstrap 5 e rota inicial

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// Importa bootstrap
+import '../js/bootstrap_js_files.js' 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
