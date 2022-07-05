99e5046b674bb1580e1368cf923cc27e81186cef
Wagner Rose <wagner.rose@gmail.com>
2022-07-05 09:55:07 -0300

    Adicionando Cabeçalho de paginas

--- spec/views/inicio/index.html.erb_spec.rb
+++ spec/views/inicio/index.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "inicio/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'Testa acesso a Página Inicio' do
+    render
+    rendered.should contain('Inicio')
+  end
+
 end
